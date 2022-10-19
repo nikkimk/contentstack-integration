4c08b533c20a13b48a8092b6d6e99ece9ea61c1e
Nikki Massaro Kauffman <nikkimk06@gmail.com>
2022-10-19 14:04:59 -0400

    init toc

--- src/course-page.js
+++ src/course-page.js
@@ -2,14 +2,15 @@
  * Copyright 2019 The Pennsylvania State University
  * @license Apache-2.0, see License.md for full text.
  */
- import { LitElement, html, css } from "lit";
+import { LitElement, html, css } from "lit";
+import { ApiBehaviors } from "./api-behaviors.js";
  /**
-  * `a11y-tab`
-  * a single tab within `a11y-tabs`
+  * `course-page`
+  * a single tab within `course-pages`
   * 
  ### Styling
  
- `<a11y-tab>` provides the following custom properties
+ `<course-page>` provides the following custom properties
  for styling:
  
  Custom property | Description | Default
@@ -17,53 +18,54 @@
   *
   * @customElement
   * @extends LitElement
-  * @see ../a11y-tabs.js
   */
- class A11yTab extends LitElement {
- 
-    /**
-     * Store the tag name to make it easier to obtain directly.
-     */
-    static get tag() {
-      return "a11y-tab";
-    }
+class CoursePage extends ApiBehaviors(LitElement) {
 
-   static get styles() {
-     return [
-       css`
-       `,
-     ];
-   }
-   // properties available to the custom element for data binding
-   static get properties() {
-     return {
-        
-     };
-   }
-   constructor() {
-     super();
-   }
-   
-   render() {
-     return html`
-     `;
-   }
- 
-   connectedCallback() {
-     super.connectedCallback();
-   }
- 
-   disconnectedCallback() {
-     super.disconnectedCallback();
-   }
-   /**
-    * @fires a11y-tab-changed
-    */
-   updated(changedProperties) {
-     changedProperties.forEach((oldValue, propName) => {
-     });
-   }
- }
- window.customElements.define(A11yTab.tag, A11yTab);
- export { A11yTab };
- 
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   */
+  static get tag() {
+    return "course-page";
+  }
+
+  static get styles() {
+    return [
+      css`
+      `,
+    ];
+  }
+  // properties available to the custom element for data binding
+  static get properties() {
+    return {
+      
+    };
+  }
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return html`
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+  updated(changedProperties) {
+    if(super.updated) super.updated(changedProperties);
+    changedProperties.forEach((oldValue, propName) => {
+    });
+  }
+  firstUpdated(changedProperties) {
+    if(super.firstUpdated) super.firstUpdated(changedProperties);
+    changedProperties.forEach((oldValue, propName) => {
+    });
+  }
+}
+window.customElements.define(CoursePage.tag, CoursePage);
+export { CoursePage };
---
+++ src/course-toc.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2019 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+ import { LitElement, html, css } from "lit";
+ import { ApiBehaviors } from "./api-behaviors.js";
+  /**
+   * `course-page`
+   * a single tab within `course-pages`
+   * 
+  ### Styling
+  
+  `<course-page>` provides the following custom properties
+  for styling:
+  
+  Custom property | Description | Default
+  ----------------|-------------|----------
+   *
+   * @customElement
+   * @extends LitElement
+   */
+ class CoursePage extends ApiBehaviors(LitElement) {
+ 
+   /**
+    * Store the tag name to make it easier to obtain directly.
+    */
+   static get tag() {
+     return "course-page";
+   }
+ 
+   static get styles() {
+     return [
+       css`
+       `,
+     ];
+   }
+   // properties available to the custom element for data binding
+   static get properties() {
+     return {
+       
+     };
+   }
+   constructor() {
+     super();
+   }
+   
+   render() {
+     return html`
+     `;
+   }
+ 
+   connectedCallback() {
+     super.connectedCallback();
+   }
+ 
+   disconnectedCallback() {
+     super.disconnectedCallback();
+   }
+   updated(changedProperties) {
+     if(super.updated) super.updated(changedProperties);
+     changedProperties.forEach((oldValue, propName) => {
+     });
+   }
+   firstUpdated(changedProperties) {
+     if(super.firstUpdated) super.firstUpdated(changedProperties);
+     changedProperties.forEach((oldValue, propName) => {
+     });
+   }
+ }
+ window.customElements.define(CoursePage.tag, CoursePage);
+ export { CoursePage };
+ 
